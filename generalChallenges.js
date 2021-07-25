0484f1566d68c25b361a16835210185cc51a2f6e
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-24 23:46:28 -0500

    challenge

--- generalChallenges.js
+++ generalChallenges.js
@@ -3,6 +3,10 @@ function triArea(a,b) {
 }
 
 function createPhoneNumber(numbers) {
+var numbers = ['0' , '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+    var formatNum = JSON.parse(numbers);
+    var phoneNo = array.map("'() +
+
 
 }
 

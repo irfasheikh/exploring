4cf17c66c601e315de07cdf04f5e9dc5235561fd
Irfa Sheikh <irfasheikh@gmail.com>
2021-07-26 21:22:12 -0500

    game

--- generalChallenges.js
+++ generalChallenges.js
@@ -5,8 +5,19 @@ function triArea(a,b) {
 function createPhoneNumber(numbers) {
 var numbers = ['0' , '1', '2', '3', '4', '5', '6', '7', '8', '9'];
     var formatNum = JSON.parse(numbers);
-    var phoneNo = array.map("'() +
+    var phoneNo = array.map("');
+
 
 
 }
+
+
+//playing js game
+// Move towards the gem.
+// Don’t touch the spikes!
+// Type your code below and click Run when you’re done.
+
+hero.moveRight();
+hero.moveDown();
+hero.moveRight();
 

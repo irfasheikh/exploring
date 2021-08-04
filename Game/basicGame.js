f2608926c531d385f0fdcc2768d1941fd2d1c292
Irfa Sheikh <irfasheikh@gmail.com>
2021-08-03 23:55:25 -0500

    beginning

--- Game/basicGame.js
+++ Game/basicGame.js
@@ -16,5 +16,13 @@ function jump() {
 }
 
 var checkHit = setInterval(function(){
+   var characterTop =
   parseInt( window.getComputedStyle(character).getPropertyValue("top")); //we parseint bc otherwise it would return px at the end
+   var blockLeft =
+       parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+   if(blockLeft < 20 && blockLeft > 0 && characterTop >=130) {
+      block.style.animation = "none";
+      block.style.display = "none";
+      alert("you lose!");
+   }
 }, 10)

2f5719849fa94d4381c9d04782e7effa7267e8dc
Irfa Sheikh <irfasheikh@gmail.com>
2021-08-10 23:56:08 -0500

    more game

---
+++ Game/MemoryGame.js
@@ -0,0 +1,20 @@
+document.addEventListener('DOMContentLoaded', () => {
+    //card options
+    const cardArray = [
+        {
+            name: 'fry',
+            img:  'images/fry.png'
+        } ,
+        {
+            name: 'helloCat',
+            img: 'images/hellocat.png'
+        } ,
+        {
+            name: 'minion',
+            img : 'images/minion.png'
+        },
+            name: 'pizza',
+
+    ]
+
+})
